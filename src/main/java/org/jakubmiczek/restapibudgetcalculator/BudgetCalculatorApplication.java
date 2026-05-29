58e7cfd13908b35ff7fb704ed7f07394effd3cb8
kubaMiczuu <kubamiczuu@gmail.com>
2026-05-29 15:46:38 +0200

    init: initial setup with docker, environment and properties

---
+++ src/main/java/org/jakubmiczek/restapibudgetcalculator/BudgetCalculatorApplication.java
@@ -0,0 +1,13 @@
+package org.jakubmiczek.restapibudgetcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jakubmiczek/restapibudgetcalculator/RestApiBudgetCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jakubmiczek.restapibudgetcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiBudgetCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
